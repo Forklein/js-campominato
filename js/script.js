2aca878eacc1c3e356102b8975e6c809348f94fe
Forklein <giuseppepisani2@gmail.com>
2021-07-14 23:13:43 +0200

    Add image in html and add event listener for play the game

--- js/script.js
+++ js/script.js
@@ -12,67 +12,77 @@ Ogni volta che l'utente sceglie un numero che non è presente tra le bombe,
 guadagna un punto e poi gli chiediamo un altro numero.
 
 Se il numero scelto dall'utente è presente tra i numeri bomba, la partita termina.
-Quando la partita termina, comunichiamo all'utente il suo punteggio. */
+Quando la partita termina, comunichiamo all'utente il suo punteggio.
+con difficoltà 0 => tra 1 e 100
+con difficoltà 1 => tra 1 e 80
+con difficoltà 2 => tra 1 e 50
+*/
 
-var fireNumber = [];
-var userNumber = [];
-var isIncluded = false;
-var punteggio = 0;
-var max;
-var mode = prompt('Inserisci la difficoltà !', 'Hard-' + 'Medium-' + 'Easy');
+//! Set variable
+const fireNumber = [];
+const userNumber = [];
+let isIncluded = false;
+let punteggio = 0;
+let max;
+let mode;
 
-while (!mode || mode.trim() === '') {
-    mode = prompt('Inserisci la difficoltà !', 'Hard-' + 'Medium-' + 'Easy');
-}
+//! Start with click image
+let imgStart = document.getElementById('img-game');
+imgStart.addEventListener('click', start);
 
-switch (mode.toLowerCase()) {
-    case 'hard':
-        max = 50;
-        break;
-    case 'medium':
-        max = 80;
-        break;
-    case 'easy':
-        max = 100;
-        break;
-    default:
-        max = 80;
-        break;
-}
+function start() {
+    do {
+        mode = prompt('Inserisci la difficoltà !', 'Hard-' + 'Medium-' + 'Easy');
+    } while (!mode || mode.trim() === '');
 
-console.log(max)
-
-while (fireNumber.length < 16) {
-    var bombNumber = randomNumber(max, 1);
-    if (!fireNumber.includes(bombNumber)) {
-        fireNumber.push(bombNumber);
+    switch (mode.toLowerCase()) {
+        case 'hard':
+            max = 50;
+            break;
+        case 'medium':
+            max = 80;
+            break;
+        case 'easy':
+            max = 100;
+            break;
+        default:
+            max = 80;
+            break;
     }
+
+    console.log(max)
+
+    while (fireNumber.length < 16) {
+        const bombNumber = randomNumber(max, 1);
+        if (!fireNumber.includes(bombNumber)) {
+            fireNumber.push(bombNumber);
+        }
+    }
+    console.log(fireNumber);
+    console.log(userNumber);
+
+    while (userNumber.length < (max - 16) && isIncluded === false) {
+        let choiceNumber;
+        do {
+            choiceNumber = parseInt(prompt('Inserisci un numero'));
+        } while (!choiceNumber || isNaN(choiceNumber) || choiceNumber > max);
+        const totalScore = max - 16;
+        if (!userNumber.includes(choiceNumber)) {
+            userNumber.push(choiceNumber);
+            console.log(userNumber);
+        } else {
+            alert('Il numero è già stato inserito!');
+        }
+        if (fireNumber.includes(choiceNumber)) {
+            isIncluded = true;
+            const resultDisplay = document.getElementById('result');
+            resultDisplay.innerHTML = '<strong>' + 'Hai perso! ' + 'Il tuo punteggio è ' + punteggio + ' su ' + totalScore + '</strong>';
+        } else {
+            punteggio++;
+        }
+    }
+    console.log(userNumber);
 }
-console.log(fireNumber);
-console.log(userNumber);
-
-while (userNumber.length < (max - 16) && isIncluded === false) {
-    var choiceNumber = parseInt(prompt('Inserisci un numero'));
-    while (!choiceNumber || isNaN(choiceNumber)) {
-        choiceNumber = parseInt(prompt('Inserisci un numero'));
-    }
-    var totalScore = max - 16;
-    if (!userNumber.includes(choiceNumber)) {
-        userNumber.push(choiceNumber);
-        console.log(userNumber);
-    } else {
-        alert('Il numero è già stato inserito!');
-    }
-    if (fireNumber.includes(choiceNumber)) {
-        isIncluded = true;
-        alert('Hai perso! ' + 'Il tuo punteggio è ' + punteggio + ' su ' + totalScore);
-    } else {
-        punteggio++;
-    }
-}
-console.log(userNumber);
-
-
 
 // Is in Array function 
 function isinArray(element, array) {
